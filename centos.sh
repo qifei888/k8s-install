3dbc6e997f279e6afd96a68c8f98398a354b2e36
mohaijiang <haijiang.mo@newtouch.cn>
2017-11-23 13:12:33 +0800

    修改docker mirror 为阿里云

--- centos.sh
+++ centos.sh
@@ -2,7 +2,7 @@
 mkdir -p /tmp/kubernetes
 cd /tmp/kubernetes
 echo "安装docker"
-curl -fsSL "https://get.docker.com/" | sh
+curl -fsSL https://get.docker.com/ | sh -s -- --mirror Aliyun
 
 echo "设置系统变量"
 cat > /etc/sysctl.d/k8s.conf <<EOF
