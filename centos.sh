c1a0a5442916ce5011cb8fb9f665407ef3130b05
mohaijiang <haijiang.mo@newtouch.cn>
2018-03-22 17:13:53 +0800

    更新kubernetes 版本为 v1.9.6

--- centos.sh
+++ centos.sh
@@ -5,6 +5,17 @@ echo "安装docker"
 # curl -fsSL https://get.docker.com/ | sh -s -- --mirror Aliyun
 yum install -y docker wget
 
+
+
+echo "设置docker daemon.json"
+cat > /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": ["https://registry.docker-cn.com"]
+}
+EOF
+
+systemctl enable docker && systemctl restart docker
+
 echo "设置系统变量"
 cat > /etc/sysctl.d/k8s.conf <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
@@ -15,25 +26,12 @@ sysctl -p /etc/sysctl.d/k8s.conf
 
 setenforce 0
 
-echo "设置docker daemon.json"
-cat > /etc/docker/daemon.json <<EOF
-{
-  "registry-mirrors": ["https://registry.docker-cn.com"],
-  "insecure-registries" : ["registry.one2.newtouch.com:5000"]
-}
-EOF
-
-systemctl enable docker && systemctl restart docker
 
 echo "安装kubernetes"
-wget -qO- http://minio.one2.newtouch.com:9000/mohaijiang/kubernetes/1.8.1/k8s_1.8.1_rpm.tar | tar -zx
+wget -qO- http://minio.test.onepoc.xonestep.com/mohaijiang/kubernetes/1.9.6/k8s_1.9.6_rpm.tar.gz | tar -zx
 rpm -Uvh *.rpm && rm -rf *.rpm
 
 
-echo "导入kubernetes镜像..."
-wget -qO- http://minio.one2.newtouch.com:9000/mohaijiang/kubernetes/1.8.1/k8s_1.8.2_image.tar.gz | tar -zx
-docker load -i k8s_1.8.2_image.tar
-rm -rf k8s_1.8.2_image.tar
 systemctl enable kubelet.service
 
 echo "kubernetes 安装成功"
@@ -41,14 +39,12 @@ echo ""
 echo "现在可以启动kubernetes"
 echo ""
 echo "master: "
-echo "           kubeadm init --kubernetes-version=v1.8.2  --pod-network-cidr=10.244.0.0/16"
+echo "           kubeadm init --config=https://raw.githubusercontent.com/mohaijiang/k8s-install/master/kubeadm/kubeadm.yaml"
 echo ""
 echo "master安装完成后，可以选择网络安装"
 echo ""
-echo "flannel: "
-echo "           kubectl apply -f https://raw.githubusercontent.com/mohaijiang/k8s-install/master/kube-flannel.yml"
 echo "calico: "
-echo "           kubectl apply -f https://raw.githubusercontent.com/mohaijiang/k8s-install/master/calico.yaml"
+echo "           kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml"
 echo ""
 echo "或者作为node节点join到kubernetes集群"
 echo ""
